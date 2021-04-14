99d2ec231507050422df4d34eb4fb1e0e0c3f825
Eze Gatica <35713330+ezegatica@users.noreply.github.com>
2021-04-14 09:13:03 -0300

    Empezando la Parte B, EJ1 hecho.

---
+++ Guias/Guia-2/src/BEjercicio1/Main.java
@@ -0,0 +1,24 @@
+package BEjercicio1;
+
+// ¿Qué salida se obtiene? ¿Por qué?
+    // Se obtiene un Distinto, porque son 2 instancias completamente diferentes, aunque el contenido sea el mismo, 
+    // estan alocadas en diferentes ubicaciones. 
+// a) Cambiando únicamente la generación de s1 y s2
+// b) Cambiando únicamente el método de comparación.
+
+public class Main {
+    public static void main(String[] args) {
+        String s1, s2;
+        s1 = new String("hola");
+        // a)
+        // s1 = s2
+        s2 = new String("hola");
+        if (s1 == s2) {
+        // b)
+        // if (s1.equals(s2)){
+            System.out.println("Son iguales");
+        } else {
+            System.out.println("Son distintos");
+        }
+    }
+}
--- Guias/Guia-2/src/Ejercicio3/Persona.java
+++ Guias/Guia-2/src/Ejercicio3/Persona.java
@@ -4,7 +4,7 @@ public class Persona {
     private String nombre;
     private String apellido;
     public Persona(String n, String a){
-        nombre = n;
-        apellido = a;
+        this.nombre = n;
+        this.apellido = a;
     }
 }
