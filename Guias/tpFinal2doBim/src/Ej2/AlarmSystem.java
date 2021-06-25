be7fcafdfb0f6a9a9e8f4495c77cb8b2728df2f7
Eze Gatica <ezequielgatica@gmail.com>
2021-06-25 00:05:15 -0300

    Funcion de llamar a los servicios de emergencia

--- Guias/tpFinal2doBim/src/Ej2/AlarmSystem.java
+++ Guias/tpFinal2doBim/src/Ej2/AlarmSystem.java
@@ -1,25 +1,47 @@
 package Ej2;
+import java.util.Arrays;
 
 public class AlarmSystem {
     private CentralAlarmSystem central;
     private String nombre;
     private int id = 0;
+    private Sensor[] sensores;
     public AlarmSystem(CentralAlarmSystem central, String nombre){
         this.central = central;
         this.nombre = nombre;
+        this.sensores = new Sensor[10];
     }
 
     public Sensor addAlarmSensor(String nombre){
+        if(id == sensores.length){
+            sensores = Arrays.copyOf(sensores, sensores.length + 10);
+          }
         Sensor sensor = new Sensor(id, nombre);
-        id++;
+        sensores[id++] = sensor;
         return sensor;
     }
 
     public void activate(){
         System.out.println(String.format("Activando alarma de sensores de %s", nombre));
+        for (Sensor sensor : sensores){
+            if(sensor != null){
+                System.out.println(String.format("%s activado)", sensor));
+            }
+        }
     }
 
     public void deactivate(){
         System.out.println(String.format("Desctivando alarma de sensores de %s", nombre));
+        for (Sensor sensor : sensores){
+            if(sensor != null){
+                System.out.println(String.format("%s desactivado)", sensor));
+            }
+        }
+    }
+    public void llamarServicios(){
+        EmergencyServices[] servicios = central.getEmergencyServices();
+        for (EmergencyServices servicio : servicios){
+            System.out.println(servicio);
+        }
     }
 }
--- Guias/tpFinal2doBim/src/Ej2_olde/Sensor.java
+++ Guias/tpFinal2doBim/src/Ej2_olde/Sensor.java
@@ -34,23 +34,3 @@ public class Sensor {
         this.activado = false;
     }
 }
-/*
-public class Sensor {
-    private final String name;
-    private final int id;
-
-    public Sensor(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public void motionDetected() {
-        System.out.println(toString() + "  detectó movimiento");
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Sensor  %s  (ID:  %d)", name, id);
-    }
-}
-*/
