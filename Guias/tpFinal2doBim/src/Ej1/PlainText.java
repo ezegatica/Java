464b4755be4079b3d33b92c025df18a12d8e65ad
Eze Gatica <ezequielgatica@gmail.com>
2021-06-25 15:00:14 -0300

    implementando HTMLText en plaintext

--- Guias/tpFinal2doBim/src/Ej1/PlainText.java
+++ Guias/tpFinal2doBim/src/Ej1/PlainText.java
@@ -1,6 +1,6 @@
 package Ej1;
 
-public class PlainText {
+public class PlainText implements HTMLText{
     private String text;
 
     public PlainText(String text) {
@@ -10,9 +10,12 @@ public class PlainText {
     public void setText(String text) {
         this.text = text;
     }
-    
+    @Override
+    public String source(){
+        return this.text.toString();
+    }
     @Override
     public String toString() {
-        return this.text;
+        return this.source();
     }
 }
